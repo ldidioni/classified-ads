d386c6e6cad85dffe213fb24528b1fcfbd03e105
Laurent Di Dionisio <laurent.di.dionisio@gmail.com>
2021-01-31 16:51:37 +0100

    Spring initializr

---
+++ src/test/java/com/ldidioni/classifiedads/ClassifiedAdsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ldidioni.classifiedads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassifiedAdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
